b37fd080723a49dc83fb137c56d5f6ca491f544f
Peyton Bischof <peyton.bischof@hotmail.com>
2023-10-05 00:15:59 -0400

    Settings Menu

--- Assets/Scripts/UI/SettingsController.cs
+++ Assets/Scripts/UI/SettingsController.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsController : MonoBehaviour
 {
     public GameObject menuUI;
     public GameObject settingsMenuUI;
+    public AudioMixer audioMixer;
+    public bool canVibrate; // attach to things that can vibrate the phone
 
     void Start()
     {
         settingsMenuUI.SetActive(false);
+        canVibrate = true;
     }
 
     public void BackButton()
     {
         menuUI.SetActive(true);
         settingsMenuUI.SetActive(false);
+    }
+
+    public void SetVibration(bool condition)
+    {
+        canVibrate = condition;
+    }
+
+    public void SetQuality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
     }
 }
