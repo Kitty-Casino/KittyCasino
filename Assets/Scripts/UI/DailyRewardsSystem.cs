9f354d5bf51c8bed536cb455de3bf04af26992a0
Peyton Bischof <peyton.bischof@hotmail.com>
2023-10-29 16:33:02 -0400

    Daily Reward Setup

---
+++ Assets/Scripts/UI/DailyRewardsSystem.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class DailyRewardsSystem : MonoBehaviour
+{
+    [Header("UI")]
+    public GameObject rewardsUI;
+    public Button claimRewardButton;
+    private CoinsController coinsController;
+
+    [Header("Rewards Data")]
+    [SerializeField] private double rewardDelay = 20f; // seconds rn, for testing only
+    [SerializeField] private float checkRewardDelay = 5f; // checks for new reward every 5 seconds
+    private bool isRewardReady;
+
+    void Start()
+    {
+        isRewardReady = false;
+        coinsController = GameObject.Find("CoinsController").GetComponent<CoinsController>();
+        claimRewardButton.onClick.RemoveAllListeners();
+
+        // check if game is opened for the first time
+        if (string.IsNullOrEmpty(PlayerPrefs.GetString("RewardClaim_DateTime")))
+        {
+            PlayerPrefs.SetString("RewardClaim_DateTime", DateTime.Now.ToString());
+        }
+
+        StopAllCoroutines();
+        StartCoroutine(CheckForReward());
+        CheckForReward();
+    }
+
+    IEnumerator CheckForReward()
+    {
+        while (true)
+        {
+            if (!isRewardReady)
+            {
+                DateTime currentDateTime = DateTime.Now;
+                DateTime rewardClaimTime = DateTime.Parse(PlayerPrefs.GetString("RewardClaim_DateTime", currentDateTime.ToString()));
+
+                // get total seconds between these times - SWITCH TO HOURS AFTER TESTING
+                double elapsedSeconds = (currentDateTime - rewardClaimTime).TotalSeconds;
+
+                if (elapsedSeconds >= rewardDelay)
+                {
+                    ActivateReward();
+                }
+                else
+                {
+                    DeactivateReward();
+                }
+            }
+
+            yield return new WaitForSeconds(checkRewardDelay);
+        }
+    }
+
+    private void ActivateReward()
+    {
+        isRewardReady = true;
+        rewardsUI.SetActive(true);
+    }
+
+    private void DeactivateReward()
+    {
+        isRewardReady = false;
+        rewardsUI.SetActive(false);
+    }
+
+    public void ClaimReward()
+    {
+        // save time of last reward claimed
+        PlayerPrefs.SetString("RewardClaim_DateTime", DateTime.Now.ToString());
+
+        coinsController.IncrementCoins(100);
+        DeactivateReward();
+    }
+}
+
+
