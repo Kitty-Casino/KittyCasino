13323ff5145dc333348cbbdee73782201f3934dc
Noah <no998698@ucf.edu>
2023-11-17 19:52:23 -0500

    MainMenuOverhaulAndWorkingSoundSettings

--- Assets/Scripts/Sound/MusicController.cs
+++ Assets/Scripts/Sound/MusicController.cs
@@ -1,50 +1,132 @@
-using JetBrains.Annotations;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MusicController : MonoBehaviour
 {
-    private AudioSource audioSource;
+    public static MusicController instance;
+
+    public AudioSource musicAudioSource;
+    public AudioSource sfxAudioSource;
     public AudioClip casinoMusic;
     public AudioClip gameMusic;
     public AudioClip shopMusic;
-    // Start is called before the first frame update
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    private float previousMusicValue = 0;
+    private float previousSFXValue = 0;
+    private string sceneName = "";
+
+    private AudioClip previousClip;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        if (!PlayerPrefs.HasKey("MusicVolume"))
+        {
+            PlayerPrefs.SetFloat("MusicVolue", 0.3f);
+        }
+        if (musicSlider != null)
+        {
+            musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        }
 
+        if (!PlayerPrefs.HasKey("SFXVolume"))
+        {
+            PlayerPrefs.SetFloat("SFXVolume", 0.3f);
+        }
+        if (sfxSlider != null)
+        {
+            sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        }
+        sceneName = SceneManager.GetActiveScene().name;
+        PlayMusic();
+    }
+
+    private void PlayMusic()
+    {
         switch (SceneManager.GetActiveScene().name)
         {
             case "Casino":
-                PlaySound(casinoMusic);
+                PlayMusic(casinoMusic);
                 break;
             case "Shop":
-                PlaySound(shopMusic);
+                PlayMusic(shopMusic);
                 break;
             case "Bartending":
-                PlaySound(gameMusic);
+                PlayMusic(gameMusic);
                 break;
             case "Blackjack":
-                PlaySound(gameMusic);
+                PlayMusic(gameMusic);
                 break;
             case "Matching":
-                PlaySound(gameMusic);
+                PlayMusic(gameMusic);
                 break;
             case "Poker":
-                PlaySound(gameMusic);
+                PlayMusic(gameMusic);
                 break;
             case "Slots":
-                PlaySound(gameMusic);
+                PlayMusic(gameMusic);
                 break;
-        }
-            
+            case "MainMenu":
+                PlayMusic(casinoMusic);
+                break;
+        }    
     }
 
     public void PlaySound(AudioClip clip)
     {
-        audioSource.PlayOneShot(clip);
+        sfxAudioSource.PlayOneShot(clip);
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        if (clip != previousClip)
+        {
+            musicAudioSource.clip = clip;
+            musicAudioSource.Play();
+
+            previousClip = clip;
+        }
+    }
+
+    public void Update()
+    {
+        if (SceneManager.GetActiveScene().name != sceneName)
+        {
+            sceneName = SceneManager.GetActiveScene().name;
+            PlayMusic();
+        }
+
+        if (musicSlider != null)
+        {
+            if (musicSlider.value != previousMusicValue)
+            {
+                musicAudioSource.volume = musicSlider.value;
+                previousMusicValue = musicSlider.value;
+                PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
+            }
+
+            if (sfxSlider.value != previousSFXValue)
+            {
+                sfxAudioSource.volume = sfxSlider.value;
+                previousSFXValue = sfxSlider.value;
+                PlayerPrefs.SetFloat("SFXVolume", sfxSlider.value);
+            }
+        }
     }
 }
